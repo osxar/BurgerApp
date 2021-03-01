cb9e20d1358553db417314bd96842894a93d1af8
oscar <55120346+osxar@users.noreply.github.com>
2021-02-28 21:32:17 -0500

    Add files via upload
    updates

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,10 +3,13 @@ import React from 'react';
 import burgerLogo from '../../assets/images/burger-logo.png';
 import classes from './Logo.css';
 
+// style={{height: props.height}}
+
 const logo = (props) => (
-    <div className={classes.Logo}>
+    <div className={classes.Logo} style={{height: props.height}}>  
         <img src={burgerLogo} alt="MyBurger" />
     </div>
 );
 
 export default logo;
+
